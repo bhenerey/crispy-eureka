dc0b0ef0f7026ec046a6c66381b099fc5aa5678a
Brian Henerey <bch820@gmail.com>
2019-11-18 17:31:08 -0600

    remove terraform code

--- terraform/workspace/example-network-mgmt/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region  = "${var.AWS_REGION}"
-  profile = "${var.vpc_attributes.workspace_profile_name}"
-}
--- terraform/workspace/example-network-mgmt/remote_state.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "${var.vpc_attributes.remote_state_bucket}"
-    key    = "${var.vpc_attributes.remote_state_key}"
-    region = "${var.AWS_REGION}"
-  }
-}
